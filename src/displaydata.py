be980eee2c686f75eede7a0a9c9dfb032071da13
vineetsaraf <tcw7711@gmail.com>
2024-10-05 16:18:00 -0400

    Fix the estimate render time actually

--- src/displaydata.py
+++ src/displaydata.py
@@ -43,7 +43,7 @@ RENDER_PATH = './render/'
 # Plotting map limits for the X and Y direction
 MIN_X, MAX_X = -100, -60   # Longitude limits
 MIN_Y, MAX_Y = 24, 40      # Latitude limits
-RES = 300
+RES = 400
 TOTAL_FRAMES = 368
 
 current_frame = 0
@@ -153,8 +153,8 @@ def update(frame):
     # FRAME += 1
     plt.savefig(f'{RENDER_PATH}my_plot{frame}.png', dpi=RES, bbox_inches='tight')
     b = time.time()
-    print(f"rendering image, time taken {b-a}")
-    print(f"est time left = {round( ((b-a) * TOTAL_FRAMES-current_frame) / 3600, 2)} hours")
+    print(f"rendering image, time taken {round(b-a,2)} seconds")
+    print(f"est time left = {round( ( (b-a) * (TOTAL_FRAMES-current_frame) ) / 3600, 2)} hours")
 
 sm = plt.cm.ScalarMappable(cmap='magma', norm=plt.Normalize(vmin=0, vmax=100))
 sm._A = []  # Dummy array for colorbar
